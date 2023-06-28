b68d0c1fcd907bb183992db8453e2f22ba75e2fe
varunkumar <varunkumarc0508@gmail.com>
2023-06-28 22:27:14 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    name: 'Varun Kumar',
+    age: 20,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPjXfCZYACFFbt5XCFxPzNMBJMYNx8AKI9xA&usqp=CAU',
+  },
+  {
+    id: 2,
+    name: 'Naveen',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 3,
+    name: 'Vasavi',
+    age: 26,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 4,
+    name: 'Harshitha',
+    age: 19,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+  {
+    id: 5,
+    name: 'Jagannadham',
+    age: 24,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  }
+];
+
